49aafde6c2a6c01529978bd87e218628d75e744d
joshuamwe <joshuajomwema2819@gmail.com>
2024-08-13 13:14:10 +0300

    q0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,30 +4,24 @@ Returns the number of subscribers for a given subreddit.
 """
 import requests
 
-
 def number_of_subscribers(subreddit):
     """
-    Returns number of subscribers for a given subreddit
+    Returns the number of subscribers for a given subreddit.
     """
     if subreddit is None or not isinstance(subreddit, str):
         return 0
-    """
-    Set the URL for the Reddit API endpoint that provides
-    information about the subreddit
-    """
+
+    # Set the URL for the Reddit API endpoint that provides information about the subreddit
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
-    """
-    Set a custom User-Agent header to identify the script
-    making the request
-    """
+    # Set a custom User-Agent header to identify the script making the request
     headers = {'User-Agent': 'MyCustomUserAgent/1.0'}
 
-    """
-    Send a GET request to the Reddit API endpoint with
-    the custom headers
-    """
+    # Send a GET request to the Reddit API endpoint with the custom headers
     response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        return 0
 
     data = response.json()
 
